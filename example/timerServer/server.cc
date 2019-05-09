6c6112b472c0f992d6fbd87d854fd7fdb19a0995
starqingzhu@163.com <starqingzhu@163.com>
2019-05-10 03:02:16 +0800

    修改使用高版本protobuf

--- example/timerServer/server.cc
+++ example/timerServer/server.cc
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <time.h>
 #include <string>
+#include <unistd.h>
+#include <pthread.h>
 #include "easy_reactor.h"
+#include "event_loop.h"
+#include <iostream>
 
 void buz1(event_loop* loop, void* usr_data)
 {
+    static unsigned time_counter = 0;
+    time_t date_time;
+    time(&date_time);
+    struct tm* tmNow = localtime(&date_time);
     FILE* fp = (FILE*)usr_data;
-    fprintf(fp, "once display ts %ld\n", time(NULL));
+    fprintf(fp, "once display ts=%s times:%u\n", asctime(tmNow),++time_counter);
     fflush(fp);
 }
 
 void buz2(event_loop* loop, void* usr_data)
 {
+    time_t date_time;
+    time(&date_time);
+    struct tm* tmNow = localtime(&date_time);
     FILE* fp = (FILE*)usr_data;
-    fprintf(fp, "always display ts %ld\n", time(NULL));
+    fprintf(fp, "always display ts %s\n", asctime(tmNow));
     fflush(fp);
 }
 
@@ -27,8 +38,13 @@ int main()
     tcp_server server(&loop, ip.c_str(), port);
 
     FILE* fp = fopen("output.log", "w");
-    loop.run_after(buz1, fp, 10);
-    loop.run_every(buz2, fp, 1, 500);
+    //loop.run_after(buz1, fp, 0,40);
+    //loop.run_every(buz2, fp, 0, 50);
+    for(int i=0;i<1000000;++i)
+    {
+        loop.run_after(buz1, fp, 0,40);
+        usleep(1);
+    }
 
     loop.process_evs();
 
